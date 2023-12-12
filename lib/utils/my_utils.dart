0724585edb6004ebf54dd4bd995b50c3d6f0d541
Niar <niary.razafinimpanana@pulse.mg>
2023-12-12 11:59:17 +0300

    Save video & audio path into a single list

--- lib/utils/my_utils.dart
+++ lib/utils/my_utils.dart
@@ -1,36 +1,32 @@
 import 'dart:io';
+import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:gallery_saver/gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 
 class MyUtils {
-  // static var pathVideos = <String>[];
-
-  static var mediasList = <String>[];
+  static var pathVideos = <String>[];
 
   static Future<void> onVideoButtonPressed(ImageSource source) async {
     final ImagePicker picker = ImagePicker();
     final XFile? file = await picker.pickVideo(
       source: source, maxDuration: const Duration(seconds: 10)
     );
-    SharedPreferences prefs = await SharedPreferences.getInstance();
     if (file != null) {
       GallerySaver.saveVideo(file.path);
       // pathVideos.add(file.path);
-      var medias = prefs.getStringList('medias');
-      medias?.add(file.path);
-      prefs.setStringList('medias', medias!);
-      mediasList = medias;
-    }
-    // for (int i = 0; i < pathVideos.length; i++) {
-    //   if(await checkFileExists(pathVideos[i])) {
-    //     print(pathVideos[i]);
-    //   }
-    // }
-
-    for (int i = 0; i < mediasList.length; i++) {
-      if(await checkFileExists(mediasList[i])) {
-        print(mediasList[i]);
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      if (prefs.getStringList('medias') == null) {
+        prefs.setStringList('medias', <String>[]);
+      }
+      var medias = prefs.getStringList('medias')!;
+      medias.add(file.path);
+      prefs.setStringList('medias', medias);
+      for (int i = 0; i < medias.length; i++) {
+        if(await checkFileExists(medias[i])) {
+          print(medias[i]);
+        }
       }
     }
   }
