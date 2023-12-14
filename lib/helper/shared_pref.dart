8efc4303f27155606cac1985d0dfe1d330711e7a
Rado Andriandrantonavalona <Rado@pulse.mg>
2023-12-14 14:03:19 +0300

    Change sharedPreferences class to a singleton

--- lib/helper/shared_pref.dart
+++ lib/helper/shared_pref.dart
@@ -1,18 +1,32 @@
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async' show Future;
 
 class SharedPrefHelper {
-  static late SharedPreferences prefs;
+  static SharedPreferences? prefs;
+  static Future<SharedPreferences> get _instance async =>
+      prefs ??= await SharedPreferences.getInstance();
 
-  static Future<void> init() async {
-    prefs = await SharedPreferences.getInstance();
+  // call this method from iniState() function of mainApp().
+  static Future<SharedPreferences> init() async {
+    prefs = await _instance;
+    return prefs!;
+  }
+
+  static String getString(String key, [String? defValue]) {
+    return prefs?.getString(key) ?? defValue ?? "";
+  }
+
+  static Future<bool> setString(String key, String value) async {
+    var prefs = await _instance;
+    return prefs.setString(key, value);
   }
 
   static void addMediaInLocalStorage(String filePath) {
-    if (prefs.getStringList('medias') == null) {
-      prefs.setStringList('medias', <String>[]);
+    if (prefs?.getStringList('medias') == null) {
+      prefs?.setStringList('medias', <String>[]);
     }
-    var medias = prefs.getStringList('medias')!;
-    medias.add(filePath);
-    prefs.setStringList('medias', medias);
+    var medias = prefs?.getStringList('medias')!;
+    medias?.add(filePath);
+    prefs?.setStringList('medias', medias ?? <String>[]);
   }
 }
